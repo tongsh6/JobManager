f6fe81b019ba2469c98917b53528bac0be866950
tongsh6 <tongsh6@qq.com>
2018-08-03 13:36:37 +0800

    项目初始化

---
+++ README.md
@@ -0,0 +1,3 @@
+JobManager
+
+该项目参考https://github.com/tjfy1992/SpringBootQuartz实现
---
+++ src/main/java/com/tongsh6/quartz/jobmanager/JobManagerApplication.java
@@ -0,0 +1,12 @@
+package com.tongsh6.quartz.jobmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobManagerApplication.class, args);
+	}
+}
